9e198f3bdafcc0994e409375774b29f4d944ce3c
sanqingqu <qusanqing@qq.com>
2020-06-28 21:02:19 +0800

    add the img_save function to the img_extract.py

--- src/camera_lidar_fusion_detection/src/img_extract.py
+++ src/camera_lidar_fusion_detection/src/img_extract.py
@@ -6,27 +6,34 @@ import numpy as np
 
 import roslib
 import rospy
-
+import time
+import os
 from sensor_msgs.msg import CompressedImage
 
 class ImageExtraction(object):
 
-    def __init__(self, sub_topic="/undistort_lower/compressed"):
+    def __init__(self, sub_topic="/undistort_lower/compressed", save_folder=None):
         self.sub_topic = sub_topic
         self.cv_bridge = CvBridge()
         self.subscriber = rospy.Subscriber(self.sub_topic,
                             CompressedImage, self.img_extraction_callback, queue_size=1)
-    
+        
+        self.save_folder = save_folder
+
     def img_extraction_callback(self, compressed_img_msg):
         # np_arr = np.fromstring(compressed_img.data, np.uint8)
         # img_np = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
         img_np = self.cv_bridge.compressed_imgmsg_to_cv2(compressed_img_msg)
+        if self.save_folder is not None:
+            img_name = compressed_img_msg.header.stamp
+            img_path = os.path.join(self.save_folder, str(img_name) + ".png")
+            cv2.imwrite(img_path, img_np)
         cv2.imshow("camera_img",img_np)
         cv2.waitKey(20)
 
 
 def main():
-    img_ext = ImageExtraction()
+    img_ext = ImageExtraction(save_folder="/home/sanqingqu/Projects/huanwei_che/tongji_sanitation_car/test_data/record_imgs")
     rospy.init_node("image_extraction", anonymous=True)
     print "init the image extraction node"
     try:
