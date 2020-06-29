9e24b55d316942a3f73431087aca32dae85dcc06
sanqingqu <qusanqing@qq.com>
2020-06-29 09:56:09 +0800

    Add BBox msg, and update the yolo_detect_publish.py

--- src/yolo_detect_pkg/src/yolo_detect_publish.py
+++ src/yolo_detect_pkg/src/yolo_detect_publish.py
@@ -7,6 +7,8 @@ from cv_bridge import CvBridge
 from rospy.numpy_msg import numpy_msg
 from yolo_detect_pkg.msg import Ints
 from yolo_detect_pkg.msg import Floats
+from yolo_detect_pkg.msg import BBoxArray
+from yolo_detect_pkg.msg import BBox
 from sensor_msgs.msg import CompressedImage
 
 class YOLODetect(object):
@@ -21,8 +23,8 @@ class YOLODetect(object):
         self.cv_bridge = CvBridge()
         self.img_subscriber = rospy.Subscriber(self.sub_img_topic, CompressedImage,
                                                 self.img_extraction_callback, queue_size=3)
-        self.bbox_pulisher = rospy.Publisher(self.pub_result_topic, numpy_msg(Ints), queue_size=2)
-        # self.bbox_pulisher = rospy.Publisher(self.pub_result_topic, numpy_msg(Floats), queue_size=2)
+
+        self.bbox_pulisher = rospy.Publisher(self.pub_result_topic, BBoxArray, queue_size=2)
         self.input_img = None
         self.bbox_np = None
     
@@ -32,21 +34,40 @@ class YOLODetect(object):
         # ----------------------------------------------------------------------------
         self.input_img = self.cv_bridge.compressed_imgmsg_to_cv2(compressed_img_msg)
         
-        self.pulish_detect_bbox()
+        self.pulish_detect_bbox(compressed_img_msg.header.stamp)
 
-    def pulish_detect_bbox(self):
+    def pulish_detect_bbox(self, msg_timestamp):
+        # init the bbox_array_msg
+        bbox_array_msg = BBoxArray()
+        bbox_array_msg.header.stamp = msg_timestamp
+        
         if self.yolo_func is not None:
             # the yolo_func output results must be np 1D array, and dtype=np.int32
             # the detection result is **bbox_np** defined as:
-            #  [x_left_corner, y_left_corner, x_right_bottom, y_right_bottom] * n
-            # the bbox_np must be resize the (n*4, ) shape like the below
+            #  [x_top_left, y_top_left, x_bottom_right, y_bottom_right] * n
             self.bbox_np = self.yolo_func(self.input_img)
-        else:
-            self.bbox_np = np.array([393, 113, 584, 471],dtype=np.int32)
-            # self.bbox_np = np.array([393, 113, 584, 471]).astype(np.float32)
-            print(self.bbox_np)
 
-        self.bbox_pulisher.publish(self.bbox_np)
+        else:
+            # this branch is just to test.
+
+            self.bbox_np = np.array([393, 113, 584, 471],dtype=np.int32)
+            self.bbox_np = np.concatenate((self.bbox_np, self.bbox_np))
+
+        bbox_array_msg.bboxes = self.get_bbox_msg(self.bbox_np)
+        self.bbox_pulisher.publish(bbox_array_msg)
+
+    def get_bbox_msg(self, bbox_np):
+        bbox_np = bbox_np.reshape(-1, 4)
+        bbox_num = len(bbox_np)
+        bbox_msg_list = []
+        for bbox in bbox_np:
+            bbox_msg = BBox()
+            bbox_msg.x_top_left = bbox[0]
+            bbox_msg.y_top_left = bbox[1]
+            bbox_msg.x_bottom_right = bbox[2]
+            bbox_msg.y_bottom_right = bbox[3]
+            bbox_msg_list.append(bbox_msg)
+        return bbox_msg_list
 
 def main():
     rospy.init_node("yolo_based_dumpster_detection", anonymous=True)
