1c2a7d6fd9865a470e157d1502155fdaca1dd172
HaiTaoWang97 <18843102609@163.com>
2020-07-30 09:30:27 -0400

    submit

---
+++ src/yolo_detect_pkg/src/dumpster_detect.py
@@ -0,0 +1,26 @@
+import torch
+import numpy as np
+from detector import Detector
+
+class ImageDumpsterDetector:
+    def __init__(self, model_def="/home/haitao/Project/tongji_sanitation_car/Yolo/config/yolov3-custom.cfg",
+                load_path="/home/haitao/Project/tongji_sanitation_car/Yolo/parameters/yolov3_ckpt_base.pth"):
+        self.yolo = Detector(torch.device("cpu"), model_def, load_path,
+                             reg_threshold=0.9, cls_threshold=0.2, nms_threshold=0.2, image_size=416)
+    def __call__(self, input_img_cur):
+        filter = [1, 2]
+        # detection result
+        print(type(input_img_cur))
+        detection = self.yolo(input_img_cur)
+        bbox_list = []
+        if detection is None: return bbox_list
+        for x1, y1, x2, y2, conf, cls_conf, cls_pred in detection:
+            #if int(cls_pred) in filter:
+            bbox = []
+            bbox.append(x1)
+            bbox.append(y1)
+            bbox.append(x2)
+            bbox.append(y2)
+            bbox = np.array(bbox, dtype=np.int32)
+            bbox_list.append(bbox)
+        return bbox_list
